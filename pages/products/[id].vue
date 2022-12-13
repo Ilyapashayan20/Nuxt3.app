4dff9e5ae71a4017376c52c7a8d88a654ed9dc76
Ilya <ilya.w.a.j@gmail.com>
2022-12-13 05:23:43 +0400

    .

---
+++ pages/products/[id].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Head>
+        <Title>Nuxt Ilya: {{product.title}}</Title>
+        <Meta name="description" :content="product.description" />
+    </Head>
+
+    <ProductDetails :product="product" />
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+  const uri = 'https://fakestoreapi.com/products/' + id
+
+  const { data: product } = await useFetch(uri, { key: id })
+  definePageMeta({
+    layout: "products",
+  })
+
+  if(!product.value){
+    throw createError({statusCode: 404, statusMessage: 'Product not found', fatal:true})
+  }
+</script>
